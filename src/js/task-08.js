4dd96433f7d43fad97e04b6f89d69bf059d41914
Vladyslav <vladislav2002vladislav2002@gmail.com>
2023-08-28 16:50:52 +0300

    changes-some-elements

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -7,17 +7,16 @@ function handleSubmit(event) {
   const form = event.currentTarget;
   const email = form.elements.email.value;
   const password = form.elements.password.value;
-  
+
   if (email === "" || password === "") {
     return alert("Please fill in all the fields!");
   } else {
     const formData = {
-        email: email,
-        password: password,
+      email: email,
+      password: password,
     };
-    
+
     console.log(formData);
     form.reset();
   }
-
 }
